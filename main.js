28a4cb515f1b69169cb7128e5bd87ab01fa08cd7
tnttgxbacminh <panh07255@gmail.com>
2025-05-19 11:27:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -464,7 +464,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 500);
     }
     
-    function startCamera(loadingElem) {
+    async function startCamera(loadingElem) {
         const videoConstraints = { facingMode: "environment" };
         const qrConfig = {
             fps: 15,
@@ -474,18 +474,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 height: { ideal: 720 }
             }
         };
-        html5QrCode
-            .start(videoConstraints, qrConfig, onScanSuccess, onScanFailure)
-            .then(() => {
-                isScanning = true;
-                if (loadingElem) loadingElem.style.display = "none";
-                console.log("Camera bắt đầu quét mã QR với facingMode: 'environment'.");
-            })
-            .catch((err) => {
-                showModal("Không truy cập được camera!", "error");
-                console.error("Lỗi khi khởi động camera với facingMode: 'environment':", err);
-            });
+
+        try {
+            await html5QrCode.start(videoConstraints, qrConfig, onScanSuccess, onScanFailure);
+            isScanning = true;
+            if (loadingElem) loadingElem.style.display = "none";
+            console.log("Camera bắt đầu quét mã QR với facingMode: 'environment'.");
+        } catch (err) {
+            showModal("Không truy cập được camera!", "error");
+            console.error("Lỗi khi khởi động camera với facingMode: 'environment':", err);
+        }
     }
+    
     function showQRInterface() {
         searchContainer.style.display = "none";
         reportContainer.style.display = "none";
